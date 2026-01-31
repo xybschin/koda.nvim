9b3d76ad9309562aa862bfce823a8e821d69078f
Oskar Nurm <19738295+oskarnurm@users.noreply.github.com>
2026-01-31 13:24:57 +0100

    feat: add more semantic highlights (#58)
    * feat(lsp): link `typemod.class.constructor` to `Function`
    
    * feat(lsp): link `typemod.class.abstract` to `Function`
    
    * feat(lsp): link `type.namespace` to `Normal`
    
    * style: indentation

--- lua/koda/groups/lsp.lua
+++ lua/koda/groups/lsp.lua
@@ -5,14 +5,17 @@ local M = {}
 function M.get_hl(c)
   -- stylua: ignore
   return {
-    DiagnosticOK           = { fg = c.success },
-    DiagnosticError        = { fg = c.danger },
-    DiagnosticWarn         = { fg = c.warning },
-    DiagnosticHint         = { fg = c.info },
-    DiagnosticInfo         = { fg = c.fg },
-    LspInlayHint           = { fg = c.comment },
-    ["@lsp.type.comment"]  = {}, -- use treesitter styles
-    ["@lsp.typemod.class.declaration"] = { link = "Function" },
+    DiagnosticOK                        = { fg = c.success },
+    DiagnosticError                     = { fg = c.danger },
+    DiagnosticWarn                      = { fg = c.warning },
+    DiagnosticHint                      = { fg = c.info },
+    DiagnosticInfo                      = { fg = c.fg },
+    LspInlayHint                        = { fg = c.comment },
+    ["@lsp.type.comment"]               = {}, -- use treesitter styles
+    ["@lsp.typemod.class.declaration"]  = { link = "Function" },
+    ["@lsp.typemod.class.constructor"]  = { link = "Function" },
+    ["@lsp.typemod.class.abstract"]     = { link = "Function" },
+    ["@lsp.type.namespace"]             = { link = "Normal" },
   }
 end
 
